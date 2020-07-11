69ff6a290f8fc8047ab4fbcbd852a0ffb71965f8
Future <40318862+Futurakos@users.noreply.github.com>
2020-07-11 17:01:31 +0300

    Update statusbot.py

--- statusbot.py
+++ statusbot.py
@@ -5,7 +5,7 @@ import ctypes
 import time
 import os
 
-token = "mfa.Jvw_OjDYP9fv98tmQZKeSpaf8PpQ1UhOttRvZWd9aaMYqLCs4Om3W6uUwttHCldB0ZfcI8F-tIaKunmlyZS4"
+token = "your token here"
 def ChangeStatus():
     try:
         session = requests.Session()
@@ -14,7 +14,7 @@ def ChangeStatus():
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/0.0.306 Chrome/78.0.3904.130 Electron/7.1.11 Safari/537.36',
             'content-type': 'application/json'
         }
-        text = random.choice(['Sleeping.', "Sleeping.", "Sleeping."])
+        text = random.choice(['Text1', "Text2", "Text3"])
         data = '{"custom_status":{"text":"' + text + '"}}'
         r = session.patch('https://discordapp.com/api/v6/users/@me/settings', headers=headers, data=data)
         if '"custom_status": {"text": "' in r.text:
@@ -26,4 +26,4 @@ def ChangeStatus():
     time.sleep(1)
 while True:
     ChangeStatus()
-    time.sleep(9000)
+    time.sleep(50) # Delay between each status change in SECONDS
